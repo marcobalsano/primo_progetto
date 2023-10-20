893bf83dc64872a5fbd92f7961fe96e00746e862
marcobalsano <marco.balsano@alessandrinimainardi.edu.it>
2023-10-20 08:34:56 +0200

    passo 12 agg

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns=[
     path('', index_seconda_app, name='index_seconda_app'),
     path('es_if', es_if, name='es_if'),
     path('if_else_elif', if_else_elif, name='if_else_elif'),
+    path('es_for', es_for, name='es_for'),
     
 
 ]
--- seconda_app/views.py
+++ seconda_app/views.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from multiprocessing import context
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def es_if(request):
@@ -16,5 +19,14 @@ def if_else_elif(request):
     }
     return render(request, "if_else_elif.html", context)
     
+
+def es_for(request):
+    context= {
+        'list1':[1, datetime.date(2023,10,20), 'never give up!'],
+        'list2':[1, datetime.date(2023,10,20), 'never give up!'],
+        'my_dict' : {'chiave1': 'Valore 1', 'chiave2': 'Valore 2'},
+    }
+    return render(request, "es_for.html", context)
+
 def index_seconda_app(request):
     return render(request, "index_seconda_app.html")
