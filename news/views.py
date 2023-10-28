962a32aa94ae0d37d19c3a88ea1b9ac76115627c
marcobannn <93837764+marcobannn@users.noreply.github.com>
2023-10-28 19:30:15 +0200

    passo 16 finito

--- news/views.py
+++ news/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Articolo, Giornalista
+
 """ def home(request):
     a=""
     g=""
@@ -15,7 +13,7 @@ from .models import Articolo, Giornalista
 
     return HttpResponse("<h1>homepage news!</h1>") """
 
-def home(request):
+""" def home(request):
     a=[]
     g=[]
     for art in Articolo.objects.all():
@@ -25,4 +23,16 @@ def home(request):
     response= str(a) + "<br>" + str(g)
     print(response)
 
-    return HttpResponse("<h1>"+response+"</h1>") 
+    return HttpResponse("<h1>"+response+"</h1>") """ 
+
+def home(request):
+        articoli = Articolo.objects.all()
+        giornalisti = Giornalista.objects.all()
+        context = {"articoli": articoli, "giornalisti": giornalisti}
+        print(context)
+        return render(request, "homepage1.html", context)
+
+def articoloDetailView(request, pk):
+        articolo = get_object_or_404(Articolo, pk=pk)
+        context = {"articolo": articolo}
+        return render(request, "articolo_detail.html", context)
