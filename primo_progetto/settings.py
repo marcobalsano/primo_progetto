af63f75efca33ea5995c28077c5fbcdea336fc5e
marcobalsano <marco.balsano@alessandrinimainardi.edu.it>
2024-04-12 11:04:50 +0200

    ok

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -134,7 +134,10 @@ MEDIA_ROOT = "uploads"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "*"
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
 ]
 
 
