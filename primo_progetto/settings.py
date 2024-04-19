36f335c880deaf948f50f848d04b8cc8e787dd61
marcobalsano <marco.balsano@alessandrinimainardi.edu.it>
2024-04-19 08:35:08 +0200

    uno

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -131,16 +131,3 @@ MEDIA_ROOT = "uploads"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-
-
-
-
-
-
-
-
-
